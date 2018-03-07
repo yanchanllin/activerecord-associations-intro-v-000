66dbff4d3979a35e856ea2996f93567f57881e41
YanC Lin <yonalin1@gmail.com>
2018-03-07 19:36:48 +0000

    automatically backed up by learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,7 +1,7 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
   def change
       create_table :songs do |t|
-        t.string :name 
+        t.string :name
         t.integer :artist_id
         t.integer :genre_id
       end
